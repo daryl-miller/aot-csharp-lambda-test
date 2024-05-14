2a4a749d9641b3596a27240ef3b63e4a7e5de236
Daryl Miller <github@darylmiller.me>
2024-05-14 13:54:34 +1000

    WIPPY

--- src/CsharpTest/CsharpTestStack.cs
+++ src/CsharpTest/CsharpTestStack.cs
@@ -1,5 +1,7 @@
 using Amazon.CDK;
 using Constructs;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.Logs;
 
 namespace CsharpTest
 {
@@ -7,7 +9,31 @@ namespace CsharpTest
     {
         internal CsharpTestStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            var buildOption = new BundlingOptions()
+            {
+                Image = Runtime.DOTNET_8.BundlingImage,
+                User = "root",
+                OutputType = BundlingOutput.ARCHIVED,
+                Command = [
+                "/bin/sh",
+                "-c",
+                " dotnet tool install -gz Amazon.Lambda.Tools"+
+                " && dotnet build"+
+                " && dotnet lambda package --output-package /asset-output/function.zip"
+                ]
+            };
+
+            new Function(this, "my-funcOne", new FunctionProps
+            {
+                Runtime = Runtime.DOTNET_8,
+                MemorySize = 128,
+                LogRetention = RetentionDays.ONE_DAY,
+                Handler = "CsharpLambda",
+                Code = Code.FromAsset("./apps/NativeAotSample", new Amazon.CDK.AWS.S3.Assets.AssetOptions
+                {
+                    Bundling = buildOption
+                }),
+            });
         }
     }
 }
